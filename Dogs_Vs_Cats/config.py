ab8b71a48da7b9e83fb59571a62f140b0e4cdc72
Louie Jin <jinluyuan@vip.qq.com>
2018-05-02 19:13:48 +0800

    update

--- Dogs_Vs_Cats/config.py
+++ Dogs_Vs_Cats/config.py
@@ -7,7 +7,7 @@ class DefaultConfig(object):
     model = 'ResNet34'  # 使用的模型，名字必须与models/__init__.py中的名字一致
 
     train_data_root = './data/train/'  # 训练集存放路径
-    test_data_root = './data/test1'  # 测试集存放路径
+    test_data_root = './data/test1/'  # 测试集存放路径
     load_model_path = None  # 加载预训练的模型的路径，为None代表不加载
 
     batch_size = 32  # batch size
@@ -18,7 +18,7 @@ class DefaultConfig(object):
     debug_file = '/tmp/debug'  # if os.path.exists(debug_file): enter ipdb
     result_file = 'result.csv'
 
-    max_epoch = 10
+    max_epoch = 20
     lr = 0.001  # initial learning rate
     lr_decay = 0.5  # when val_loss increase, lr = lr*lr_decay
     weight_decay = 0e-5  # 损失函数
--- Dogs_Vs_Cats/main.py
+++ Dogs_Vs_Cats/main.py
@@ -3,7 +3,7 @@ from config import opt
 import os
 import torch as t
 import models
-from data.dataset import DogCat
+from dataset.dataset import DogCat
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from torchnet import meter
@@ -12,27 +12,25 @@ from tqdm import tqdm
 
 def test(**kwargs):
     opt.parse(kwargs)
-    # import ipdb;
-    # ipdb.set_trace()
+
     # configure model
     model = getattr(models, opt.model)().eval()
     if opt.load_model_path:
         model.load(opt.load_model_path)
-    if opt.use_gpu: model.cuda()
+    if opt.use_gpu:
+        model.cuda()
 
     # data
-    train_data = DogCat(opt.test_data_root,test=True)
-    test_dataloader = DataLoader(train_data,batch_size=opt.batch_size,shuffle=False,num_workers=opt.num_workers)
+    train_data = DogCat(opt.test_data_root, test=True)
+    test_dataloader = DataLoader(train_data, opt.batch_size, shuffle=False, num_workers=opt.num_workers)
     results = []
-    for ii,(data,path) in tqdm(enumerate(test_dataloader)):
-        input = t.autograd.Variable(data,volatile = True)
-        if opt.use_gpu: input = input.cuda()
+    for ii, (data, path) in tqdm(enumerate(test_dataloader)):
+        input = t.autograd.Variable(data, volatile=True)
+        if opt.use_gpu:
+            input = input.cuda()
         score = model(input)
-        probability = t.nn.functional.softmax(score)[:,0].data.tolist()
-        # label = score.max(dim = 1)[1].data.tolist()
-        
+        probability = t.nn.functional.softmax(score)[:, 0].data.tolist()
         batch_results = [(path_,probability_) for path_,probability_ in zip(path,probability) ]
-
         results += batch_results
     write_csv(results,opt.result_file)
 
@@ -53,20 +51,19 @@ def train(**kwargs):
     model = getattr(models, opt.model)()
     if opt.load_model_path:
         model.load(opt.load_model_path)
-    if opt.use_gpu: model.cuda()
+    if opt.use_gpu:
+        model.cuda()
 
     # step2: data
-    train_data = DogCat(opt.train_data_root,train=True)
-    val_data = DogCat(opt.train_data_root,train=False)
-    train_dataloader = DataLoader(train_data,opt.batch_size,
-                        shuffle=True,num_workers=opt.num_workers)
-    val_dataloader = DataLoader(val_data,opt.batch_size,
-                        shuffle=False,num_workers=opt.num_workers)
+    train_data = DogCat(opt.train_data_root, train=True)
+    val_data = DogCat(opt.train_data_root, train=False)
+    train_dataloader = DataLoader(train_data, opt.batch_size, shuffle=True, num_workers=opt.num_workers)
+    val_dataloader = DataLoader(val_data, opt.batch_size, shuffle=False, num_workers=opt.num_workers)
     
     # step3: criterion and optimizer
     criterion = t.nn.CrossEntropyLoss()
     lr = opt.lr
-    optimizer = t.optim.Adam(model.parameters(),lr = lr,weight_decay = opt.weight_decay)
+    optimizer = t.optim.Adam(model.parameters(), lr=lr, weight_decay=opt.weight_decay)
         
     # step4: meters
     loss_meter = meter.AverageValueMeter()
@@ -75,11 +72,10 @@ def train(**kwargs):
 
     # train
     for epoch in range(opt.max_epoch):
-        
         loss_meter.reset()
         confusion_matrix.reset()
 
-        for ii,(data,label) in tqdm(enumerate(train_dataloader)):
+        for ii, (data, label) in tqdm(enumerate(train_dataloader)):
 
             # train model 
             input = Variable(data)
@@ -90,16 +86,15 @@ def train(**kwargs):
 
             optimizer.zero_grad()
             score = model(input)
-            loss = criterion(score,target)
+            loss = criterion(score, target)
             loss.backward()
             optimizer.step()
-            
             
             # meters update and visualize
             loss_meter.add(loss.data[0])
             confusion_matrix.add(score.data, target.data)
 
-            if ii%opt.print_freq==opt.print_freq-1:
+            if ii % opt.print_freq == opt.print_freq - 1:
                 vis.plot('loss', loss_meter.value()[0])
                 
                 # 进入debug模式
@@ -107,15 +102,14 @@ def train(**kwargs):
                     import ipdb;
                     ipdb.set_trace()
 
-
         model.save()
 
         # validate and visualize
-        val_cm,val_accuracy = val(model,val_dataloader)
-
-        vis.plot('val_accuracy',val_accuracy)
+        val_cm, val_accuracy = val(model, val_dataloader)
+        vis.plot('val_accuracy', val_accuracy)
         vis.log("epoch:{epoch},lr:{lr},loss:{loss},train_cm:{train_cm},val_cm:{val_cm}".format(
-                    epoch = epoch,loss = loss_meter.value()[0],val_cm = str(val_cm.value()),train_cm=str(confusion_matrix.value()),lr=lr))
+            epoch=epoch, loss=loss_meter.value()[0], val_cm=str(val_cm.value()),
+            train_cm=str(confusion_matrix.value()), lr=lr))
         
         # update learning rate
         if loss_meter.value()[0] > previous_loss:          
@@ -123,11 +117,10 @@ def train(**kwargs):
             # 第二种降低学习率的方法:不会有moment等信息的丢失
             for param_group in optimizer.param_groups:
                 param_group['lr'] = lr
-        
 
         previous_loss = loss_meter.value()[0]
 
-def val(model,dataloader):
+def val(model, dataloader):
     """
     计算模型在验证集上的准确率等信息
     """
@@ -150,7 +143,6 @@ def help():
     """
     打印帮助的信息： python file.py help
     """
-    
     print("""
     usage : python file.py <function> [--args=value]
     <function> := train | test | help
@@ -158,7 +150,8 @@ def help():
             python {0} train --env='env0701' --lr=0.01
             python {0} test --dataset='path/to/dataset/root/'
             python {0} help
-    avaiable args:""".format(__file__))
+    avaiable args:
+    """.format(__file__))
 
     from inspect import getsource
     source = (getsource(opt.__class__))
